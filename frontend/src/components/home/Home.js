c2c0fd0ac2e80153821efe16ab6ec3a7022a4953
kamrulcoder <kamrulcoder@gmail.com>
2022-08-01 10:51:51 +0600

    Component folder setup and scss file structure create

---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+export default function componentName() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <div className="row">
+      <div className="col-6">
+        <h2>Check  Row </h2>
+      </div>
+    </div>
+    </>
+  );
+}
