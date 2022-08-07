0992f579d85d930094d9c6f255b582be9f67a7cd
kamrulcoder <kamrulcoder@gmail.com>
2022-08-07 12:42:08 +0600

    Dashboard Index Component Design Completed

--- frontend/src/components/dashboard/Dashborad.js
+++ frontend/src/components/dashboard/Dashborad.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 import DashboradNavbar from './DashboradNavbar'
 import Sidebar from './Sidebar';
 import { Switch, Route } from "react-router-dom";
+import DashboradIndex from './DashboradIndex';
 
 const Dashborad = () => {
   return (
@@ -13,6 +14,10 @@ const Dashborad = () => {
       <DashboradNavbar />
       <div className="dashborad-main-content">
         <Sidebar />
+        <Switch>
+          <Route path="/dashborad" component={DashboradIndex} exact />
+
+        </Switch>
       </div>
     </div>
   )
