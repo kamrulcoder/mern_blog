c0655d27562fa6957ab306298db3d07dd2a712a5
kamrulcoder <kamrulcoder@gmail.com>
2022-08-08 09:21:03 +0600

    React Redux Setup System

---
+++ frontend/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
+import ThunkMiddleware from "redux-thunk";
+
+const  rootReducer = combineReducers({
+
+})
+
+
+const middleware = [ThunkMiddleware]
+
+
+const store = createStore(rootReducer , compose(applyMiddleware(...middleware) ,
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+export default store ; 
